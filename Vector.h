8592f464570433d1b73be5124fc6246b8a0e57d6
erazer <151334231+shiwei0206@users.noreply.github.com>
2024-09-20 00:54:32 +0800

    Update Vector.h

--- Vector.h
+++ Vector.h
@@ -19,11 +19,11 @@ protected:
 public:
 //构造函数
 	Vector(int c = DEFAULT_CAPACITY, int s = 0, T v = 0);//容量为c，规模为s，所有元素初始为v
-	{_elem = new T[_capacity = c];for (_size = 0; _size < s, _elem[_size++] = v);}//s <= c
+	{ _elem = new T[_capacity = c]; for (_size = 0; _size < s; _elem[_size++] = v); }//s <= c
 	Vector(T const* A, Rank lo, Rank hi){copyFrom(A, lo, hi);}//数组区间复制
 	Vector(T const* A, Rank n){copyFrom(A, 0, n);}//数组整体复制
 	Vector(Vector<T> const& V, Rank lo, Rank hi) { copyFrom(V._elem, lo, hi); }//向量区间复制
-	Vector(Vector<T> const$ V) { copyFrom(V._elem, 0, V._size); }//向量整体复制
+	Vector(Vector<T> const& V) { copyFrom(V._elem, 0, V._size); }//向量整体复制
 //析构函数
 	~Vector() { delete[] _elem; }//释放内部空间
 //只读访问接口
@@ -34,7 +34,7 @@ public:
 	Rank find(T const& e, Rank lo, Rank hi) const;//无序向量区间查找
 	Rank search(T const& e) const//有序向量整体查找
 	{return(0 >= _size) ? -1 : search(e, 0, _size);}
-	Rank search(T const& e, Rank lo, Rank hi) comst；//有序向量整体查找
+	Rank search(T const& e, Rank lo, Rank hi) const；//有序向量整体查找
 //可写访问接口
 	T& operator[](Rank r) const;//重载下标操作符，可以类似于数组形式引出各元素
 	Vector<T>& operator=(Vector<T> const&);//重载赋值操作符，以便直接克隆向量
@@ -52,24 +52,36 @@ public:
 	void traverse(void (*)(T&));//遍历（使用函数指针，只读或局部性修改）
 	template <typename VST> void traverse(VST&);//遍历（使用函数对象，可全局性修改）
 };//Vector
-//基于复制的构造方法
+//基于复制的向量构造器
 template <typename T>//元素类型
 void Vector<T>::copyFrom(T const* A, Rank lo, Rank hi) {//以数组区间A【lo,hi】为蓝本复制向量
 	_elem = new T[_capacity = 2 * （hi - lo)]; _size = 0;//分配空间，规模清零
 	while (lo < hi)//A【lo，hi】内的元素逐一
 		_elem[_size++] = A[lo++];//复制至_elem[0,hi-lo]
 }
+//重载向量赋值操作符
 template <typename T> Vector<T>& Vector<T>::operator=(Vector<T> const& V) {//重载赋值操作符
 	if (_elem) delete[] _elem;//释放原有内容
 	copyFrom(V._elem, 0, V.size());//整体复制
 	return *this;//返回当前对象的应用，以便链式赋值
 }
-//扩容
+//向量内部数组动态扩容算法expand()
 template <typename T> void Vector<T>::expand() {//向量空间不足时扩容
 	if (_size < _capacity) return;//尚未满员时，不必扩容
 	if (_capacity < DEFAULT_CAPACITY) _capacity = DEFAULT_CAPACITY;//不低于最小容量
 	T* oldElem = _elem; _elem = new T[_capacity <<= 1];//容量加倍
 	for (int i = 0; i < _size; i++)
-		_elem[i] = oldElem;//复制原向量内容（T为基本类型，或已重载赋值操作符‘=’）
+		_elem[i] = oldElem[i];//复制原向量内容（T为基本类型，或已重载赋值操作符‘=’）
 	delete [] oldElem;//释放原空间
 }
+//向量内部功能shrink()
+template <typename T> void Vector<T>::shrink() {//填装因子过小时压缩向量所占空间
+	if (_capacity < DEFAULT_CAPACITY << 1) return;//不致收缩到DEFAULT_CAPACITY以下
+	if (_size << 2 > _capacity)return;//以25%为界
+	T* oldElem = _elem; _elem = new T[_capacity >>= 1];//容量减半
+	for (int i = 0; i < _size; i++)_elem[i] = oldElem[i];//复制原向量内容
+	delete[] oldElem;//释放原空间
+}
+//重载向量操作符[]
+template <typename T> T& Vector<T>::operator[](Rank r) const
+{return _elem[r];}
