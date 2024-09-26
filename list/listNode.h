91a704f1a9be221ffbb74515ed76d37a804e8991
erazer <shi010300@qq.com>
2024-09-26 23:54:24 +0800

    new

---
+++ list/listNode.h
@@ -0,0 +1,18 @@
+// 3.1列表节点模板类
+typename int Rank;                    // 秩
+#define ListNodePosi(T) ListNode<T> * // 列表节点位置
+
+template <typename T>
+struct ListNode // 列表节点类模板(以双向链表形式表现)
+{
+    T date;               // 数值
+    ListNodePosi(T) pred; // 前驱
+    ListNodePosi(T) succ; // 后继
+    // 构造函数
+    ListNode() {} // 针对header和trailer的构造
+    ListNode(T e, ListNodeposi(T) p = NULL, ListNodeposi(T) s = NULL)
+        : date(e), pred(p), succ(s) {} // 默认构造器
+    // 操作接口
+    ListNodePosi(T) insertAsPred(T const &e); // 紧靠当前节点之前插入新节点
+    ListNodePosi(T) insertAsSucc(T const &e); // 紧靠当前节点之后插入新节点
+};
