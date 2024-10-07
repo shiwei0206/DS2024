1a437f3ba23cf955331e24f8fb6da74a92bc4ea2
erazer <shi010300@qq.com>
2024-10-07 14:27:24 +0800

    10.7

--- exp1.cpp
+++ exp1.cpp
@@ -63,47 +63,47 @@ void print(Complex &e)
 // 随机生成乱序复数向量
 Vector<Complex> generateRandomVector(int n)
 {
-    Vector<Complex> vec;
+    Vector<Complex> A;
     srand(time(0));
     int uniqueSize = n - 2;
-    Vector<Complex> vec1;
+    Vector<Complex> A1;
     for (int i = 0; i < uniqueSize; ++i)
     {
         double real = rand() % 10; // 实部在 0 到 9 之间
         double imag = rand() % 10; // 虚部在 0 到 9 之间
-        vec1.insert(Complex(real, imag));
-        vec.insert(Complex(real, imag)); // 插入到最终的向量中
+        A1.insert(Complex(real, imag));
+        A.insert(Complex(real, imag)); // 插入到最终的向量中
     }
     for (int i = uniqueSize; i < n; ++i)
     {
         int randomIndex = rand() % uniqueSize; // 从唯一元素中随机选择
-        vec.insert(vec1[randomIndex]);
+        A.insert(A1[randomIndex]);
     }
-    return vec;
+    return A;
 }
 Vector<Complex> generateOrderedVector(int n)
 {
-    Vector<Complex> vec;
+    Vector<Complex> A;
     for (int i = 0; i < n; ++i)
-    {vec.insert(static_cast<double>(i), static_cast<double>(i));}
-    return vec;
+    {A.insert(static_cast<double>(i), static_cast<double>(i));}
+    return A;
 }
 // 生成逆序复数向量
 Vector<Complex> generateReverseOrderedVector(int n)
 {
-    Vector<Complex> vec;
+    Vector<Complex> A;
     for (int i = n; i > 0; --i)
-    { vec.insert(static_cast<double>(i), static_cast<double>(i));}
-    return vec;
+    { A.insert(static_cast<double>(i), static_cast<double>(i));}
+    return A;
 }
 // 记录排序时间并输出结果
-void measureSortingTime(const string &sortType, Vector<Complex> vec)
+void measureSortingTime(const string &sortType, Vector<Complex> A)
 {
     auto start = chrono::high_resolution_clock::now();
     if (sortType == "起泡排序")
-    { vec.bubbleSort(0, vec.size()-1);}
+    { A.bubbleSort(0, A.size()-1);}
     else if (sortType == "归并排序")
-    {vec.mergeSort(0, vec.size()-1);}
+    {A.mergeSort(0, A.size()-1);}
     auto end = chrono::high_resolution_clock::now();
     chrono::duration<double, micro> duration_us = end - start;
     cout << sortType<<" time: " << duration_us.count() << " us" << endl;
@@ -111,42 +111,42 @@ void measureSortingTime(const string &sortType, Vector<Complex> vec)
 int main()
 {
     // 1. 生成一个无序的复数向量
-    int n = 1000;
-    Vector<Complex> vec = generateRandomVector(n);
+    int n = 10;
+    Vector<Complex> A = generateRandomVector(n);
     cout << "无序复数向量:\n";
-    vec.traverse(print);
+    A.traverse(print);
     cout << endl;
     // 测试置乱
-    vec.unsort(0, vec.size()-1);
+    A.unsort(0, A.size()-1);
     cout << "置乱后的复数向量:\n";
-    vec.traverse(print);cout << endl;
+    A.traverse(print);cout << endl;
     // // 测试查找
     Complex searchComplex(1, 2); // 用以查找的复数
-    Rank foundIndex = vec.find(searchComplex);
+    Rank foundIndex = A.find(searchComplex);
     if (foundIndex != -1)
     { cout << "找到元素 " << searchComplex << " 在索引 " << foundIndex << endl;}
     else
     {cout << "未找到元素 " << searchComplex << endl;}
     // 测试插入
-    vec.insert(0, Complex(1, 2));
+    A.insert(0, Complex(1, 2));
     cout << "插入元素 (1, 2) 后的复数向量:\n";
-    vec.traverse(print);cout << endl;
+    A.traverse(print);cout << endl;
     // 测试删除
-    vec.remove(0); // 删除插入的元素
+    A.remove(0); // 删除插入的元素
     cout << "删除元素后复数向量:\n";
-    vec.traverse(print);cout << endl;
+    A.traverse(print);cout << endl;
     // 唯一化复数向量
-    vec.deduplicate();
+    A.deduplicate();
     cout << "唯一化后的复数向量:\n";
-    vec.traverse(print);cout << endl;
+    A.traverse(print);cout << endl;
     // 排序
-    Sort1(vec);
+    Sort1(A);
     cout << "排序后的复数向量:\n";
-    vec.traverse(print);cout << endl;
+    A.traverse(print);cout << endl;
     // 测试区间查找
     double m1 = 3.0;  // 设置模的下界
     double m2 = 8.0; // 设置模的上界
-    Vector<Complex> foundInRange = findInRange(vec, m1, m2);
+    Vector<Complex> foundInRange = findInRange(A, m1, m2);
     cout << "在区间 [" << m1 << ", " << m2 << "):的元素\n";
     foundInRange.traverse(print);cout<<endl;
     // 测试顺序
@@ -166,6 +166,6 @@ int main()
     Vector<Complex> reverseVec = generateReverseOrderedVector(n);
     Vector<Complex> reverseVec1 = reverseVec;
     measureSortingTime("起泡排序", reverseVec);
-    measureSortingTime("归并排序", reverseVec);
+    measureSortingTime("归并排序", reverseVec1);
     return 0;
  }
