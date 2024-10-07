2f5f2de3840fd0bdc7990fc76925da0ad1e7f168
erazer <shi010300@qq.com>
2024-10-07 18:16:53 +0800

    v

--- exp1/Vector.cpp
+++ exp1/Vector.cpp
@@ -396,22 +396,20 @@ void Vector<T>::mergeSort(Rank lo, Rank hi)
 }
 
 // 2.29有序向量的二路归并
-template <typename T> // 有序向量的归并
-void Vector<T>::merge(Rank lo, Rank mi, Rank hi)
-{					   // 以mi为界、各自有序的子向量[lo,mi]和[mi,hi]
-	T *A = _elem + lo; // 合并后的向量A[0, hi - lo] = _elem[lo,mi]
-	int lb = mi - lo;
-	T *B = new T[lb]; // 前子向量B[0,lb] = _elem[lo,mi]
-	for (Rank i = 0; i < lb; B[i] = A[i++])
-		; // 复制前子向量
-	int lc = hi - mi;
-	T *C = _elem + mi; // 后子向量c[0,lc] = _elem[mi,hi]
-	for (Rank i = 0, j = 0, k = 0; (j < lb) || (k < lc);)
-	{ // 将b[j]和C[k]中的小者续至末尾
-		if ((j < lb) && (!(k < lc) || (B[j] <= C[k])))
-			A[i++] = B[j++];
-		if ((k < lc) && (!(j < lb) || (C[k] < B[j])))
-			A[i++] = C[k++];
-	}
-	//delete[] B; // 释放临时空间B
-} // 归并后得到完整的有序向量[lo,hi]
+template <typename T>
+void Vector<T>::merge(Rank lo, Rank mi, Rank hi) {
+    T* A = _elem + lo;
+    int lb = mi - lo;
+    T* B = new T[lb];
+    for (Rank i = 0; i < lb; i++) B[i] = A[i]; // 复制前子向量
+    
+    int lc = hi - mi;
+    T* C = _elem + mi; // 后子向量C[0, lc)就地
+    
+    for (Rank i = 0, j = 0, k = 0; j < lb; ) { // 归并：反复从B和C中取出更小者
+        if (k >= lc || B[j] < C[k]) A[i++] = B[j++];
+        else A[i++] = C[k++];
+    }
+    delete [] B; // 释放临时空间B
+}
+ // 归并后得到完整的有序向量[lo,hi]
