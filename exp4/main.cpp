ba1bccc99507704d341cc24764bd3521dd5de400
erazer <shi010300@qq.com>
2024-12-02 21:30:44 +0800

    new

--- exp4/main.cpp
+++ exp4/main.cpp
@@ -1,27 +1,22 @@
 #include "../unite/Graph/Graph.h"
-
 int main() {
-    // 创建一个图实例
     GraphMatrix<string, string> g;
-
     // 添加顶点
     g.insert("A");
     g.insert("B");
     g.insert("C");
     g.insert("D");
     g.insert("E");
-
     // 添加边 (顶点1, 顶点2, 权重)
-    g.insert("AB", 1, 0, 1);
-    g.insert("AC", 4, 0, 2);
+    g.insert("AB", 1, 0, 3);
+    g.insert("AC", 4, 0, 1);
     g.insert("BC", 2, 1, 2);
-    g.insert("BD", 5, 1, 3);
-    g.insert("CD", 1, 2, 3);
-    g.insert("DE", 3, 3, 4);
+    g.insert("BD", 5, 1, 5);
+    g.insert("CD", 1, 2, 4);
+    g.insert("DE", 3, 3, 6);
     g.insert("CE", 6, 2, 4);
-
-    // 测试深度优先搜索（DFS）
-    cout << "DFS starting from vertex A:" << endl;
+    // 测试DFS
+    cout << "DFS starting from  A:" << endl;
     g.dfs(0); // 从顶点 'A' 开始深度优先搜索（0 表示顶点 A）
     for (int i = 0; i < g.n; i++) {
         cout << "Vertex " << g.vertex(i) << ": ";
@@ -29,23 +24,19 @@ int main() {
         cout << "fTime = " << g.fTime(i) << ", ";
         cout << "parent = " << (g.parent(i) != -1 ? g.vertex(g.parent(i)) : "None") << endl;
     }
-
-    // 测试广度优先搜索（BFS）
-    cout << "\nBFS starting from vertex A:" << endl;
+    // 测试BFS
+    cout << "\nBFS starting from  A:" << endl;
     g.bfs(0);
     for (int i = 0; i < g.n; i++) {
         cout << "Vertex " << g.vertex(i) << ": ";
         cout << "dTime = " << g.dTime(i) << ", ";
         cout << "parent = " << (g.parent(i) != -1 ? g.vertex(g.parent(i)) : "None") << endl;
     }
-
-    // 执行 Dijkstra 算法，寻找从 A（顶点0）到其他顶点的最短路径
-    cout << "\nDijkstra's Shortest Path from vertex A:" << endl;
+    // 测试 Dijkstra 算法，寻找从 A（顶点0）到其他顶点的最短路径
+    cout << "\nDijkstra  A:" << endl;
     g.dijkstra(0);
-
-    // 执行 Prim 算法，构建最小生成树
-    cout << "\nPrim's Minimum Spanning Tree starting from vertex A:" << endl;
+    // 测试 Prim 算法，构建最小生成树
+    cout << "\nPrim  A:" << endl;
     g.prim(0);
-
     return 0;
 }
--- unite/Graph/Graph.h
+++ unite/Graph/Graph.h
@@ -6,10 +6,9 @@
 #include <functional>
 using namespace std;
 
-// 图状态和边的状态定义
+// 图数据结构的实现
 typedef enum { UNDISCOVERED, DISCOVERED, VISITED } VStatus;
 typedef enum { E_UNDISCOVERED, TREE, CROSS, FORWARD, BACKWARD } EStatus;
-
 // 顶点类
 template<typename Tv>
 struct Vertex {
@@ -19,23 +18,18 @@ struct Vertex {
     int dTime, fTime;
     int parent;
     int priority;
-
     Vertex(Tv const& d = Tv()) 
         : data(d), inDegree(0), outDegree(0), 
           status(UNDISCOVERED), dTime(-1), fTime(-1), parent(-1), priority(INT_MAX) {}
 };
-
 // 边类
 template <typename Te>
 struct Edge {
-    Te data;  // 边的权重或其他信息
-    int weight;  // 边的权重
-    EStatus status;  // 边的状态
-
-    Edge(Te const& d = Te(), int w = 0)
-        : data(d), weight(w), status(E_UNDISCOVERED) {}
+    Te data;  
+    int weight;  
+    EStatus status;  
+    Edge(Te const& d = Te(), int w = 0) : data(d), weight(w), status(E_UNDISCOVERED) {}
 };
-
 // 基础图类
 template<typename Tv, typename Te>
 class Graph {
@@ -43,12 +37,9 @@ protected:
     virtual void reset() = 0;
     virtual void BFS(int, int&) = 0;
     virtual void DFS(int, int&) = 0;
-     virtual void PFS(int, function<void(int, int)> updateFunc) = 0; 
-
 public:
     int n;  // 顶点数
     int e;  // 边数
-
     virtual int insert(Tv const&) = 0;
     virtual Tv remove(int) = 0;
     virtual Tv& vertex(int) = 0;
@@ -61,7 +52,6 @@ public:
     virtual int& fTime(int) = 0;
     virtual int& parent(int) = 0;
     virtual int& priority(int) = 0;
-
     virtual bool exists(int, int) = 0;
     virtual void insert(Te const&, int, int, int) = 0;
     virtual Te remove(int, int) = 0;    
@@ -73,14 +63,12 @@ public:
     void prim(int);
     void dijkstra(int);
 };
-
 // 邻接矩阵图类实现
 template<typename Tv, typename Te>
 class GraphMatrix : public Graph<Tv, Te> {
 private:
     vector<Vertex<Tv>> V;  // 顶点数组
     vector<vector<Edge<Te>*>> E;  // 邻接矩阵（每个位置存储指向边的指针）
-
 public:
     GraphMatrix() { this->n = this->e = 0; }
     ~GraphMatrix() {
@@ -88,7 +76,6 @@ public:
             for (int k = 0; k < this->n; k++)
                 delete E[j][k];
     }
-
     virtual Tv& vertex(int i) { return V[i].data; } 
     virtual int inDegree(int i) { return V[i].inDegree; } 
     virtual int outDegree(int i) { return V[i].outDegree; } 
@@ -107,7 +94,6 @@ public:
         V.push_back(Vertex<Tv>(vertex)); 
         return this->n - 1;
     }
-
     virtual Tv remove(int i) { 
         for (int j = 0; j < this->n; j++) 
             if (exists(i, j)) { delete E[i][j]; V[j].inDegree--; } 
@@ -119,13 +105,10 @@ public:
         Tv vBak = vertex(i); 
         return vBak; 
     }
-
     virtual bool exists(int i, int j) { return (0 <= i) && (i < this->n) && (0 <= j) && (j < this->n) && E[i][j] != nullptr; }
-
     virtual EStatus& status(int i, int j) { return E[i][j]->status; } 
     virtual Te& edge(int i, int j) { return E[i][j]->data; } 
     virtual int& weight(int i, int j) { return E[i][j]->weight; }
-
     virtual void insert(Te const& edge, int w, int i, int j) { 
         if (exists(i, j)) return; 
         E[i][j] = new Edge<Te>(edge, w); 
@@ -133,7 +116,6 @@ public:
         V[i].outDegree++; 
         V[j].inDegree++; 
     }
-
     virtual Te remove(int i, int j) { 
         Te eBak = edge(i, j); 
         delete E[i][j]; 
@@ -143,7 +125,6 @@ public:
         V[j].inDegree--; 
         return eBak; 
     }
-
     // BFS 实现
     void BFS(int v, int& clock) { 
         queue<int> Q;
@@ -164,7 +145,6 @@ public:
             status(v) = VISITED; 
         }
     }
-
     void bfs(int s) { 
         reset(); 
         int clock = 0; 
@@ -174,7 +154,6 @@ public:
                 BFS(v, clock);  
         } while (s != (v = (++v % this->n))); 
     }
-
     // DFS 实现
     void DFS(int v, int& clock) {
         dTime(v) = ++clock; 
@@ -194,7 +173,6 @@ public:
         status(v) = VISITED; 
         fTime(v) = ++clock;
     }
-
     void dfs(int s) {
         reset(); 
         int clock = 0;
@@ -204,7 +182,6 @@ public:
                 DFS(v, clock);
         } while (s != (v = (++v % this->n)));
     }
-
     // Dijkstra 算法
     void dijkstra(int s) {
         reset(); 
@@ -221,23 +198,18 @@ public:
                     }
                 }
             }
-
             int minPriority = INT_MAX;
             int minVertex = -1;
-
             for (int i = 0; i < this->n; i++) {
                 if (status(i) == UNDISCOVERED && priority(i) < minPriority) {
                     minPriority = priority(i);
                     minVertex = i;
                 }
             }
-
             if (minVertex == -1) break;
-
             s = minVertex;
             status(s) = VISITED;
         }
-
         // 输出最短路径
         for (int i = 0; i < this->n; i++) {
             if (i != s) {
@@ -253,50 +225,18 @@ public:
         }
     }
 
-    // 实现 PFS 函数
-    void PFS(int s, function<void(int, int)> updateFunc) override {
-        reset();
-        priority(s) = 0;
-        status(s) = VISITED;
-        parent(s) = -1;
-
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.push({0, s});
-
-        while (!pq.empty()) {
-            int u = pq.top().second;
-            pq.pop();
-
-            for (int v = firstNbr(u); v >= 0; v = nextNbr(u, v)) {
-                if (status(v) == UNDISCOVERED) {
-                    int weightEdge = weight(u, v);
-                    if (priority(v) > weightEdge) {
-                        priority(v) = weightEdge;
-                        parent(v) = u;
-                        pq.push({priority(v), v});
-                        updateFunc(u, v);  // 调用更新函数
-                    }
-                }
-            }
-            status(u) = VISITED;
-        }
-    }
-
-    // Prim 算法（最小生成树）
+    // Prim 算法（最小生成树
     void prim(int s) {
         reset();
         priority(s) = 0;
         status(s) = VISITED;
         parent(s) = -1;
-
         // 使用优先队列来选择当前边权最小的点
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         pq.push({0, s});
-
         while (!pq.empty()) {
             int u = pq.top().second;
             pq.pop();
-
             // 遍历所有邻接的顶点
             for (int v = firstNbr(u); v >= 0; v = nextNbr(u, v)) {
                 if (status(v) == UNDISCOVERED) {
@@ -310,7 +250,6 @@ public:
             }
             status(u) = VISITED;
         }
-
         // 输出最小生成树
         cout << "Minimum Spanning Tree using Prim's Algorithm:" << endl;
         for (int i = 0; i < this->n; i++) {
@@ -319,7 +258,6 @@ public:
             }
         }
     }
-
     // 重置图的状态
     void reset() {
         for (int i = 0; i < this->n; i++) {
